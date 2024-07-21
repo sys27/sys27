84fc94799ca16ac2b426b8f7beb72427144b7897
Dmytro Kyshchenko <sys2712@gmail.com>
2024-07-21 21:11:41 +0300

    Temporary integrate giscus.

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,11 +1,17 @@
 import { htmlToJsx } from "../../util/jsx"
+import Comments from "../Comments"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
   const content = htmlToJsx(fileData.filePath!, tree)
   const classes: string[] = fileData.frontmatter?.cssclasses ?? []
   const classString = ["popover-hint", ...classes].join(" ")
-  return <article class={classString}>{content}</article>
+  return (
+    <>
+      <article class={classString}>{content}</article>
+      <Comments />
+    </>
+  )
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
