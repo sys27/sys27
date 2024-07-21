4dd61438738cb7109f7f2316d54124c41b71af4a
Dmytro Kyshchenko <sys2712@gmail.com>
2024-07-21 21:11:41 +0300

    Temporary integrate giscus.

---
+++ quartz/components/Comments.tsx
@@ -0,0 +1,26 @@
+const Comments = () => {
+  return (
+    <>
+      <div class="giscus"></div>
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="sys27/sys27"
+        data-repo-id="R_kgDOLw20Lw"
+        data-category="General"
+        data-category-id="DIC_kwDOLw20L84Ce0bA"
+        data-mapping="title"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="dark_tritanopia"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async
+      ></script>
+    </>
+  )
+}
+
+export default Comments
